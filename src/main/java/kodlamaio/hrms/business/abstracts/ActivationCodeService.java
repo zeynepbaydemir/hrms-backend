3449028af92a12e639e13c2c6de1b27a20bf5924
Zeynep Baydemir <zeynepbydemir@gmail.com>
2021-06-07 17:19:05 +0300

    delete

--- src/main/java/kodlamaio/hrms/business/abstracts/ActivationCodeService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import kodlamaio.hrms.core.utilities.results.Result;
-import kodlamaio.hrms.entities.concretes.ActivationCode;
-
-public interface ActivationCodeService {
-
-	Result add(ActivationCode code);
-}
--- src/main/java/kodlamaio/hrms/core/utilities/adapters/ValidationService.java
+++
@@ -1,7 +0,0 @@
-package kodlamaio.hrms.core.utilities.adapters;
-
-
-public interface ValidationService {
-
-	boolean validateByMernis(long nationalIdentity, String firstName, String lastName, int dateOfBirth);
-}
